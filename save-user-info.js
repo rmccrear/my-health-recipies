be94b75d57cddf5da49ef29da17618d95d8d0566
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-09-16 17:52:59 -0700

    put updates in update-headers

--- save-user-info.js
+++ save-user-info.js
@@ -20,20 +20,3 @@ function handleSaveData() {
 }
 
 saveButton.addEventListener("click", handleSaveData);
-
-function renderNavInfo() {
-  let userNameNavItem = document.getElementById("user-name")
-  let emailNavItem = document.getElementById("user-email")
-
-  // Step 1: retrieve data from localStorage
-  let myEmail = localStorage.getItem("email");
-  let myName = localStorage.getItem("fullName");
-  console.log(myName, myEmail);
-
-  // Step 2: put data into the DOM
-  userNameNavItem.textContent = myName;
-  emailNavItem.textContent = myEmail;
-}
-
-// Show the saved data when page loads.
-renderNavInfo();
